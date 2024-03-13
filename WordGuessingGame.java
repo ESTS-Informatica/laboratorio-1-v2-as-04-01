ec1ba88f7df62240eb4b54eb443ecd61e4490d05
Rodrigo-Siquennique <202100649@estudantes.ips.pt>
2024-03-13 09:43:59 +0000

    Nível 2 - alínea 2 e 3

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -19,6 +19,7 @@ public class WordGuessingGame
     {
         this.hiddenWord = "abc";
         this.guessedWord = "___";
+        this.reader = new InputReader();
     }
 
     String getHiddenWord()
@@ -41,4 +42,37 @@ public class WordGuessingGame
         System.out.println("Palavrs adivinhada: " + this.guessedWord);
     }
     
+    public void play()
+    {
+        showWelcome();
+
+        showGuessedWord();
+
+        guess(reader.getChar(guessedWord));
+
+        if (guessedWord.equals(hiddenWord)) {
+            showResult();
+        }
+
+    }
+
+    private void showWelcome()
+    {
+        System.err.println("Welcome!");
+    }
+
+    private void guess(char character)
+    {
+        for(char letter: this.hiddenWord.toCharArray())
+        {
+            if (character == letter) {
+                this.guessedWord.replace(letter, character);
+            } 
+        }
+    }
+
+    private void showResult()
+    {
+        System.err.println("Número de tentativas: " + this.numberOfTries);
+    }
 }
