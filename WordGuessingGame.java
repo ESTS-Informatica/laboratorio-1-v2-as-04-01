8e3913d027f02ceef89435c2c0551c9debce8fed
Rodrigo-Siquennique <202100649@estudantes.ips.pt>
2024-03-13 09:11:16 +0000

    Nível 1 - alínea 1

---
+++ WordGuessingGame.java
@@ -0,0 +1,19 @@
+
+/**
+ * Escreva uma descrição da classe WordGuessingGame aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class WordGuessingGame
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String hiddenWord;
+    private String guessedWord;
+    private int numberOfTries;
+
+
+    
+
+    
+}
